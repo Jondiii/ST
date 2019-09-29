93f1354ec4088c8a169bedde95c1add3a0d5753d
Usuario <Usuario@DESKTOP-L9KE9N2>
2019-09-29 12:33:38 +0200

    PIPO

--- ST/src/Prueba/PorFavorFunciona.java
+++ ST/src/Prueba/PorFavorFunciona.java
@@ -1,10 +1,18 @@
 package Prueba;
 
 public class PorFavorFunciona {
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Jondiii/ST.git
 	//Soy Jon s<fg<
 	private boolean alo;
 	
 	public PorFavorFunciona() {
 		alo = true;
 	}
+=======
+	dfbfedbfb 
+>>>>>>> b355f02 PIPO
+
+
+
+VIVA PIKACHU WIIIIII
 }
