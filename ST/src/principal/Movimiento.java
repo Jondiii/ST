50867667ee92fdb0993a63fc32310b2057ddf5e9
Jon Díaz <jondi@opendeusto.es>
2019-10-03 17:22:26 +0200

    Añadidos métodos básicos de la clase Movimiento.

--- ST/src/principal/Movimiento.java
+++ ST/src/principal/Movimiento.java
@@ -1,5 +1,71 @@
 package principal;
 
 public class Movimiento {
+	
+	private String nombre;
+	private Tipo tipo;		//Cada movimiento pertenece a un único tipo.
+	private int potencia;	//Potencia del ataque.
+	private int precisión;	//Probabilidad de que el movimiento acierte.
+	private int pp;			//Puntos de Poder. Número máximo de veces que se puede usar un movimiento.
+	private int ppRestantes;//Número de veces restante que se puede usar el movimiento.
+	
+	public Movimiento(String nombre, Tipo tipo, int potencia, int precisión, int pp) {
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.potencia = potencia;
+		this.precisión = precisión;
+		this.pp = pp;
+		this.ppRestantes = pp;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public int getPrecisión() {
+		return precisión;
+	}
+
+	public void setPrecisión(int precisión) {
+		this.precisión = precisión;
+	}
+
+	public int getPp() {
+		return pp;
+	}
+
+	public void setPp(int pp) {
+		this.pp = pp;
+	}
+
+	public int getPpRestantes() {
+		return ppRestantes;
+	}
+
+	public void setPpRestantes(int ppRestantes) {
+		this.ppRestantes = ppRestantes;
+	}
+	
+	
 
 }
--- ST/src/principal/Tipo.java
+++ ST/src/principal/Tipo.java
@@ -4,11 +4,10 @@ public enum Tipo {
 	
 	BICHO(1), DRAGON(2), ELECTRICO(3), HADA(4), LUCHA(5), FUEGO(6), VOLADOR(7), FANTASMA(8),
 	PLANTA(9), TIERRA(10), HIELO(11), NORMAL(12), VENENO(13), PSIQUICO(14), ROCA(15), ACERO(16), 
-	AGUA(17), SINIESTRO(18)
-
-	;
-	private int id;
+	AGUA(17), SINIESTRO(18);
 	
+	private int id;
+
 	Tipo(int id){
 		this.id = id;
 	}
