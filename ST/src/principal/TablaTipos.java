61ff050b019f61179f57e32ff84ef03e90c62937
Jon Díaz <jondi@opendeusto.es>
2019-10-05 13:57:40 +0200

    Tabla de tipos para efectividades.

---
+++ ST/src/principal/TablaTipos.java
@@ -0,0 +1,72 @@
+package principal;
+
+public class TablaTipos {
+
+	/*
+    FILAS = Pokemon atacante
+    COLUMNAS = Pokemon que recibe el ataque
+    
+    Significados de los números:
+    	- 0: el atacante hace un daño nulo al oponente.
+        - 0.5: el atacante hace la mitad de daño al oponente.
+        - 1: el atacante hace un daño neutro al oponente.
+        - 2: el atacante hace el doble de daño al oponente.
+	 */
+	
+	public static double[][] efectividadesPokemon = {
+	        
+	        /*ACERO   AGUA   BICHO   DRAGON   ELÉC   FANT   FUEGO   HADA   HIELO   LUCHA   NORMAL   PLANTA   PSI   ROCA   SINIE  TIERRA   VENENO   VOLADOR   */
+	        
+	        { 0.5   , 0.5  ,   1   ,   1    , 0.5  ,  1   ,  0.5   ,  2  ,   2   ,   1   ,    1    ,   1   ,  1   ,  2  ,   1   ,   1   ,    1   ,   1},   //ACERO
+	        {  1    , 0.5  ,   1   ,  0.5   ,  1   ,  1   ,   2    ,  1  ,   1   ,   1   ,    1    ,  0.5  ,  1   ,  2  ,   1   ,   2   ,    1   ,   1},   //AGUA
+	        { 0.5   ,  1   ,   1   ,   1    ,  1   , 0.5  ,  0.5   , 0.5 ,   1   ,  0.5  ,    1    ,   2   ,  2   ,  1  ,   2   ,   1   ,   0.5  ,  0.5},  //BICHO
+	        { 0.5   ,  1   ,   1   ,   2    ,  1   ,  1   ,   1    ,  0  ,   1   ,   1   ,    1    ,   1   ,  1   ,  1  ,   1   ,   1   ,    1   ,   1},   // DRAGÓN
+	        {  1    ,  2   ,   1   ,  0.5   , 0.5  ,  1   ,   1    ,  1  ,   1   ,   1   ,    1    ,  0.5  ,  1   ,  1  ,   1   ,   0   ,    1   ,   2},   // ELÉCTRICO
+	        {  1    ,  1   ,   1   ,   1    ,  1   ,  2   ,   1    ,  1  ,   1   ,   1   ,    0    ,   1   ,  2   ,  1  ,  0.5  ,   1   ,    1   ,   1},   // FANTASMA
+	        {  2    , 0.5  ,   2   ,  0.5   ,  1   ,  1   ,  0.5   ,  1  ,   2   ,   1   ,    1    ,   2   ,  1   , 0.5 ,   1   ,   1   ,    1   ,   1},   // FUEGO
+	        { 0.5   ,  1   ,   1   ,   2    ,  1   ,  1   ,  0.5   ,  1  ,   1   ,   2   ,    1    ,   1   ,  1   ,  1  ,   2   ,   1   ,   0.5  ,   1},   // HADA
+	        { 0.5   , 0.5  ,   1   ,   2    ,  1   ,  1   ,  0.5   ,  1  ,  0.5  ,   1   ,    1    ,   2   ,  1   ,  1  ,   1   ,   2   ,    1   ,   2},   // HIELO
+	        {  2    ,  1   ,  0.5  ,   1    ,  1   ,  0   ,   1    , 0.5 ,   2   ,   1   ,    2    ,   1   , 0.5  ,  2  ,   2   ,   1   ,   0.5  ,  0.5},  // LUCHA
+	        { 0.5   ,  1   ,   1   ,   1    ,  1   ,  0   ,   1    ,  1  ,   1   ,   1   ,    1    ,   1   ,  1   , 0.5 ,   1   ,   1   ,    1   ,   1},   // NORMAL
+	        { 0.5   ,  2   ,  0.5  ,  0.5   ,  1   ,  1   ,  0.5   ,  1  ,   1   ,   1   ,    1    ,  0.5  ,  1   ,  2  ,   1   ,   2   ,   0.5  ,  0.5},  // PLANTA
+	        { 0.5   ,  1   ,   1   ,   1    ,  1   ,  1   ,   1    ,  1  ,   1   ,   2   ,    1    ,   1   , 0.5  ,  1  ,   0   ,   1   ,    2   ,   1},   // PSÍQUICO
+	        { 0.5   ,  1   ,   2   ,   1    ,  1   ,  1   ,   2    ,  1  ,   2   ,  0.5  ,    1    ,   1   ,  1   ,  1  ,   1   ,  0.5  ,    1   ,   2},   // ROCA
+	        {  1    ,  1   ,   1   ,   1    ,  1   ,  2   ,   1    , 0.5 ,   1   ,  0.5  ,    1    ,   1   ,  2   ,  1  ,  0.5  ,   1   ,    1   ,   1},   // SINIESTRO
+	        {  2    ,  1   ,  0.5  ,   1    ,  2   ,  1   ,   2    ,  1  ,   1   ,   1   ,    1    ,  0.5  ,  1   ,  2  ,   1   ,   1   ,    2   ,   0},   // TIERRA
+	        {  0    ,  1   ,   1   ,   1    ,  1   , 0.5  ,   1    ,  2  ,   1   ,   1   ,    1    ,   2   ,  1   , 0.5 ,   1   ,  0.5  ,   0.5  ,   1},   // VENENO
+	        { 0.5   ,  1   ,   2   ,   1    , 0.5  ,  1   ,   1    ,  1  ,   1   ,   2   ,    1    ,   2   ,  1   , 0.5 ,   1   ,   1   ,    1   ,   1}    // VOLADOR
+	    };
+	
+	public static float calculaEficacia(Pokemon defensor, Movimiento mov) {
+		float eficacia = (float)efectividadesPokemon[mov.getTipo().getId()][defensor.getTipos().get(0).getId()];
+		if(defensor.getTipos().get(1).equals(null)) { mensajeEficacia(eficacia); return eficacia; }
+		eficacia *= (float)efectividadesPokemon[mov.getTipo().getId()][defensor.getTipos().get(1).getId()];
+		mensajeEficacia(eficacia);
+		return eficacia;
+	}
+	
+	/**
+	 * Método que le da al cuadro de texto el mensaje que tiene que mostrar en función de la eficacia del movimiento realizado.
+	 * @param eficacia
+	 * @return
+	 */
+	public static String mensajeEficacia(double eficacia) {
+		if (eficacia == 0) {
+			return "No ha afectado al objetivo...";
+		}
+		if (eficacia == 0.25) {
+			return "El movimiento ha sido muy poco eficaz...";
+		}
+		if (eficacia == 0.5) {
+			return "El movimiento no ha sido muy eficaz...";
+		}
+		if (eficacia == 1) {
+			return "";
+		}
+		if (eficacia == 2) {
+			return "¡El movimiento es muy eficaz!";
+		}
+		return "¡¡El movimiento ha sido supereficaz!!";
+	}
+	
+}
--- ST/src/principal/Tipo.java
+++ ST/src/principal/Tipo.java
@@ -3,14 +3,17 @@ package principal;
 import java.awt.Color;
 
 public enum Tipo {
-	
-	BICHO(1, new Color(132,214,68)), DRAGON(2, new Color (70,33,129)), ELECTRICO(3, Color.YELLOW), 
-	HADA(4, Color.PINK), LUCHA(5, new Color (240,131,40)), FUEGO(6, Color.RED),
-	VOLADOR(7, new Color (170,220,227)), FANTASMA(8, new Color(93,38,118)),
-	PLANTA(9, Color.GREEN), TIERRA(10, new Color(184,119,65)), HIELO(11, new Color (23,220,249)), 
-	NORMAL(12, new Color(201,200,200)), VENENO(13, new Color (147,21,180)), PSIQUICO(14, new Color(250,10,156)), 
-	ROCA(15, new Color (144,86,29)), ACERO(16, Color.GRAY), 
-	AGUA(17, Color.BLUE), SINIESTRO(18, new Color(66,64,62));
+	/**
+	 * Ordenados los tipos para que sea más fácil copiar la tabla de tipos que está
+	 * en google drive.
+	 */
+	ACERO(0, Color.GRAY), AGUA(1, Color.BLUE),BICHO(2, new Color(132,214,68)),
+	DRAGON(3, new Color (70,33,129)), ELECTRICO(4, Color.YELLOW),FANTASMA(5, new Color(93,38,118)),
+	FUEGO(6, Color.RED), HADA(7, Color.PINK), HIELO(8, new Color (23,220,249)), 
+	LUCHA(9, new Color (240,131,40)), NORMAL(10, new Color(201,200,200)),
+	PLANTA(11, Color.GREEN), PSIQUICO(12, new Color(250,10,156)),ROCA(13, new Color (144,86,29)),
+	SINIESTRO(14, new Color(66,64,62)), TIERRA(15, new Color(184,119,65)), 
+	VENENO(16, new Color (147,21,180)), VOLADOR(17, new Color (170,220,227));
 	
 	private int id;
 	private Color color;
