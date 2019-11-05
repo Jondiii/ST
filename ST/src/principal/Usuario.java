2b2cdc350007dc43038e26c7afb706c495082b4b
AneCapde <anecapdevila@gmail.com>
2019-11-05 12:01:14 +0100

    Añadida la clase udsuario

---
+++ ST/src/principal/Usuario.java
@@ -0,0 +1,66 @@
+package principal;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Usuario {
+	private String nombre;
+	private String contra;
+	private String nickname;
+	private int puntuacion;
+	private int partidasGanadas;
+	private int partidasPerdidas;
+	private TreeMap<Integer, ArrayList<Pokemon>> pokes_entrenador; // he puesto que los equipos esten
+	// ordenados por numero pero se puede cambiar
+	
+	public Usuario(String nombre, String contra, String nickname, int puntuacion, int partidasGanadas,
+			int partidasPerdidas) {
+		this.nombre = nombre;
+		this.contra = contra;
+		this.nickname = nickname;
+		this.puntuacion = puntuacion;
+		this.partidasGanadas = partidasGanadas;
+		this.partidasPerdidas = partidasPerdidas;
+		pokes_entrenador = new TreeMap<Integer, ArrayList<Pokemon>>();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContra() {
+		return contra;
+	}
+	public void setContra(String contra) {
+		this.contra = contra;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+	public int getPartidasGanadas() {
+		return partidasGanadas;
+	}
+	public void setPartidasGanadas(int partidasGanadas) {
+		this.partidasGanadas = partidasGanadas;
+	}
+	public int getPartidasPerdidas() {
+		return partidasPerdidas;
+	}
+	public void setPartidasPerdidas(int partidasPerdidas) {
+		this.partidasPerdidas = partidasPerdidas;
+	}
+	public void anyadirEquipo(ArrayList<Pokemon> nuevo_equipo) {
+		int numero = pokes_entrenador.lastKey();
+		pokes_entrenador.put(numero ++, nuevo_equipo);
+	}
+}
