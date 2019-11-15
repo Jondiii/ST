fdd48ac55a6253907c605b8ccabd8bb5d7f45675
Jon Díaz <jondi@opendeusto.es>
2019-11-15 11:51:39 +0100

    Reorganizada la ventana de registrarse. Se ve el sprite elegido.

--- ST/src/ventanas/VentanaInicio.java
+++ ST/src/ventanas/VentanaInicio.java
@@ -20,6 +20,7 @@ import java.util.Properties;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -173,33 +174,101 @@ public class VentanaInicio extends JFrame {
 		bRegister.addActionListener(new ActionListener() {
 			
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				ArrayList<String> listaEntrenadores = new ArrayList<String>();
+			public void actionPerformed(ActionEvent arg0) {//Falta reordenar la ventana para que sea bonita y
+														   //que al hacer click en registrarse compruebe que no haya otro user igual y que se añadan
+														   //los datos del user a la BD.
 				
-				File folder = new File("/img/entrenadores/");//Esto no chuta.
-				System.out.println(folder.isDirectory());
-				File[] listOfFiles = folder.listFiles();
-				System.out.println(listOfFiles);
-
-				for (int i = 0; i < listOfFiles.length; i++) {
-				   listaEntrenadores.add(listOfFiles[i].getName());
-				   System.out.println(listOfFiles[i].getName());
-				}
+				ArrayList<String> listaEntrenadores = new ArrayList<String>();
+				String[] ArrayEntrenadores = new String[5]; //Pasamos de un array list a un array porque java no lo pilla si no.
+				
+//				File folder = new File("/img/entrenadores");//Esto no chuta.
+//				File[] listOfFiles = folder.listFiles();
+//
+//				for (int i = 0; i < listOfFiles.length; i++) {
+//					String nombreRuta = listOfFiles[i].getName();
+//					String nombreEntrenador = nombreRuta.split(".")[0];
+//					listaEntrenadores.add(nombreEntrenador);
+//				}
+//				
+//				String[] ArrayEntrenadores = new String[listaEntrenadores.size()]; //Pasamos de un array list a un array porque java no lo pilla si no.
+//				for (int i = 0; i < listaEntrenadores.size(); i++) {
+//					ArrayEntrenadores[i] = listaEntrenadores.get(i);
+//				}
+				
+				ArrayEntrenadores[0] = "Blasco"; //Pongo estos porque como lo de arriba no va pues con algo habrá que probar
+				ArrayEntrenadores[1] = "Israel";
+				ArrayEntrenadores[2] = "Máximo";
+				ArrayEntrenadores[3] = "Catleya";
+				ArrayEntrenadores[4] = "Lárrayas";
 				
 				JDialog d = new JDialog();
-				JPanel pUsuario = new JPanel(new FlowLayout());
+				JPanel pDatos = new JPanel(new FlowLayout());
 				JTextField areaUsername = new JTextField(14);
-				pUsuario.add(new JLabel("Usuario: "));
-				pUsuario.add(areaUsername);
-				JPanel pContraseña = new JPanel(new FlowLayout());
+				pDatos.add(new JLabel("Usuario: "));
+				pDatos.add(areaUsername);
 				JPasswordField areaContra = new JPasswordField(14);
-				pContraseña.add(new JLabel("Contraseña: "));
-				pContraseña.add(areaContra);
+				pDatos.add(new JLabel("Contraseña: "));
+				pDatos.add(areaContra);
+				pDatos.add(new JLabel("Elige tu avatar: "));
+				JComboBox<String> entrenadores = new JComboBox<String>(ArrayEntrenadores);
+				pDatos.add(entrenadores);
+				
+				ImageIcon icono_1 = new ImageIcon(getClass().getResource("/img/entrenadores/unknown.png"));
+				ImageIcon icono_2 = new ImageIcon(icono_1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT));
+				JLabel labelSprite = new JLabel(icono_2);
+				JPanel pSprite = new JPanel();
+				pSprite.add(labelSprite);
+				
+				entrenadores.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						ImageIcon icono_1 = new ImageIcon(getClass().getResource("/img/entrenadores/" + entrenadores.getSelectedItem() + ".png"));
+						ImageIcon icono_2 = new ImageIcon(icono_1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT));
+						labelSprite.setIcon(icono_2);
+						pSprite.removeAll();
+						pSprite.add(labelSprite);
+						d.revalidate();
+					}
+				});
+				
 				JPanel panelOk = new JPanel();
-				JButton bConfirmar = new JButton("OK");
-				panelOk.add(bConfirmar);
+				JButton bConfirmar = new JButton("Registrarse");
+				JButton bCancelar = new JButton("Cancelar");
+				panelOk.add(bConfirmar, BorderLayout.WEST);
+				panelOk.add(bCancelar, BorderLayout.EAST);
 				
+				JLabel info = new JLabel(); //Label que mostrará mensaje de error si el usuario introducido ya existe, el campo
+											//usuario está vacío o el campo contraseña está vacío.
+				pDatos.add(info);
 				
+				d.add(pDatos, BorderLayout.CENTER);
+				d.add(panelOk, BorderLayout.SOUTH);
+				d.add(pSprite, BorderLayout.EAST);
+				d.setSize(500, 200);
+				d.setVisible(true);
+				d.setLocation(vi.getLocation());
+				d.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				
+				bConfirmar.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {//Eto no va
+						if (areaUsername.getText().trim() == "" || areaUsername.getText().isEmpty()) {
+							info.setText("Error, el usuario está vacío.");
+							d.revalidate();
+							return;
+						}
+						String contraseña = new String(areaContra.getPassword());
+						if( contraseña.trim() == ""  || contraseña.isEmpty()) {
+							info.setText("Error, la contraseña está vacía.");
+							d.revalidate();
+							return;
+						}
+						
+						
+					}
+				});
 			}
 		});
 		
