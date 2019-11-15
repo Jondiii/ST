e530e91f44318916d0a5bae8a81b06c26593ffb1
AneCapde <anecapdevila@gmail.com>
2019-11-15 18:14:51 +0100

    Funciona las img-entrenador, he cambiado el layout espero que os guste
    y reconoce los espacios en blanco y manda el mensaje de error.

--- ST/src/ventanas/VentanaInicio.java
+++ ST/src/ventanas/VentanaInicio.java
@@ -1,6 +1,7 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,6 +28,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import main.Main;
 import principal.Combate;
@@ -179,8 +181,15 @@ public class VentanaInicio extends JFrame {
 														   //los datos del user a la BD.
 				
 				ArrayList<String> listaEntrenadores = new ArrayList<String>();
-				String[] ArrayEntrenadores = new String[5]; //Pasamos de un array list a un array porque java no lo pilla si no.
+				 //Pasamos de un array list a un array porque java no lo pilla si no.
 				
+				File fInic = new File("src/img/entrenadores/");
+				 if (fInic.isDirectory()) {
+					 for( File f : fInic.listFiles() ) {
+						 	String nombreRuta = f.getName();
+							listaEntrenadores.add(nombreRuta);
+				 		}
+				 }
 //				File folder = new File("/img/entrenadores");//Esto no chuta.
 //				File[] listOfFiles = folder.listFiles();
 //
@@ -190,28 +199,48 @@ public class VentanaInicio extends JFrame {
 //					listaEntrenadores.add(nombreEntrenador);
 //				}
 //				
-//				String[] ArrayEntrenadores = new String[listaEntrenadores.size()]; //Pasamos de un array list a un array porque java no lo pilla si no.
-//				for (int i = 0; i < listaEntrenadores.size(); i++) {
-//					ArrayEntrenadores[i] = listaEntrenadores.get(i);
-//				}
-				
-				ArrayEntrenadores[0] = "Blasco"; //Pongo estos porque como lo de arriba no va pues con algo habrá que probar
-				ArrayEntrenadores[1] = "Israel";
-				ArrayEntrenadores[2] = "Máximo";
-				ArrayEntrenadores[3] = "Catleya";
-				ArrayEntrenadores[4] = "Lárrayas";
+				String[] ArrayEntrenadores = new String[listaEntrenadores.size()]; //Pasamos de un array list a un array porque java no lo pilla si no.
+				for (int i = 0; i < listaEntrenadores.size(); i++) {
+					String nombreEntrenador = listaEntrenadores.get(i).replaceAll(".png", "");
+					ArrayEntrenadores[i] = nombreEntrenador;
+				}
+//				
+//				ArrayEntrenadores[0] = "Blasco"; //Pongo estos porque como lo de arriba no va pues con algo habrá que probar
+//				ArrayEntrenadores[1] = "Israel";
+//				ArrayEntrenadores[2] = "Máximo";
+//				ArrayEntrenadores[3] = "Catleya";
+//				ArrayEntrenadores[4] = "Lárrayas";
 				
 				JDialog d = new JDialog();
-				JPanel pDatos = new JPanel(new FlowLayout());
+				JPanel pCentral = new JPanel (new BorderLayout());
+				
+				JPanel pDatos = new JPanel(new GridLayout(4, 0));
+				pDatos.setPreferredSize(new Dimension(500, 200));
+				JPanel pUsuario = new JPanel();
+				pUsuario.setPreferredSize(new Dimension(300, 25));
+				JPanel pContra = new JPanel();
+				pContra.setPreferredSize(new Dimension(300, 25));
+				JPanel pEntrenador = new JPanel();
+				pEntrenador.setPreferredSize(new Dimension(300, 40));
 				JTextField areaUsername = new JTextField(14);
-				pDatos.add(new JLabel("Usuario: "));
-				pDatos.add(areaUsername);
+				pUsuario.add(new JLabel("Usuario: "));
+				pUsuario.add(areaUsername);
 				JPasswordField areaContra = new JPasswordField(14);
-				pDatos.add(new JLabel("Contraseña: "));
-				pDatos.add(areaContra);
-				pDatos.add(new JLabel("Elige tu avatar: "));
+				pContra.add(new JLabel("Contraseña: "));
+				pContra.add(areaContra);
+				pEntrenador.add(new JLabel("Elige tu avatar: "));
 				JComboBox<String> entrenadores = new JComboBox<String>(ArrayEntrenadores);
-				pDatos.add(entrenadores);
+				pEntrenador.add(entrenadores);
+				pDatos.add(pUsuario);
+				pDatos.add(pContra);
+				pDatos.add(pEntrenador);
+				
+				JLabel info = new JLabel(""); //Label que mostrará mensaje de error si el usuario introducido ya existe, el campo
+							//usuario está vacío o el campo contraseña está vacío.
+				info.setHorizontalAlignment(JLabel.CENTER);
+				pDatos.add(info);
+
+				
 				
 				ImageIcon icono_1 = new ImageIcon(getClass().getResource("/img/entrenadores/unknown.png"));
 				ImageIcon icono_2 = new ImageIcon(icono_1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT));
@@ -238,14 +267,12 @@ public class VentanaInicio extends JFrame {
 				panelOk.add(bConfirmar, BorderLayout.WEST);
 				panelOk.add(bCancelar, BorderLayout.EAST);
 				
-				JLabel info = new JLabel(); //Label que mostrará mensaje de error si el usuario introducido ya existe, el campo
-											//usuario está vacío o el campo contraseña está vacío.
-				pDatos.add(info);
 				
-				d.add(pDatos, BorderLayout.CENTER);
+				pCentral.add(pDatos, BorderLayout.CENTER);
+				d.add(pCentral, BorderLayout.CENTER);
 				d.add(panelOk, BorderLayout.SOUTH);
 				d.add(pSprite, BorderLayout.EAST);
-				d.setSize(500, 200);
+				d.setSize(500, 225);
 				d.setVisible(true);
 				d.setLocation(vi.getLocation());
 				d.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -254,13 +281,13 @@ public class VentanaInicio extends JFrame {
 					
 					@Override
 					public void actionPerformed(ActionEvent e) {//Eto no va
-						if (areaUsername.getText().trim() == "" || areaUsername.getText().isEmpty()) {
+						if (areaUsername.getText().trim().equals("")|| areaUsername.getText().isEmpty()) {
 							info.setText("Error, el usuario está vacío.");
 							d.revalidate();
 							return;
 						}
 						String contraseña = new String(areaContra.getPassword());
-						if( contraseña.trim() == ""  || contraseña.isEmpty()) {
+						if( contraseña.trim().equals("")  || contraseña.isEmpty()) {
 							info.setText("Error, la contraseña está vacía.");
 							d.revalidate();
 							return;
