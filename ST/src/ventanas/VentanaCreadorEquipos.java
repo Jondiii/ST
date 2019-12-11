f2c4ce9c199afb174f3700994500809e1cf50e53
Jon Díaz <jondi@opendeusto.es>
2019-12-11 12:01:39 +0100

    Se carga la lista de movimientos desde la base de datos

--- ST/src/ventanas/VentanaCreadorEquipos.java
+++ ST/src/ventanas/VentanaCreadorEquipos.java
@@ -10,6 +10,11 @@ import java.awt.event.FocusListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -23,6 +28,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
+import database.BaseDatosPoke;
+
 public class VentanaCreadorEquipos extends JFrame {
 	
 	VentanaCreadorEquipos vc;
@@ -31,7 +38,7 @@ public class VentanaCreadorEquipos extends JFrame {
 		vc = this;
 		setTitle("Creador de equipos");
 		setLocation(200, 100);
-		setSize(500, 500);
+		setSize(1000, 1000);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 		//Se crean los componentes básicos de la ventana
@@ -48,15 +55,19 @@ public class VentanaCreadorEquipos extends JFrame {
 		
 		JPanel pPoke = new JPanel();
 		JPanel pIzq = new JPanel();
-		JPanel pCentro = new JPanel(new GridLayout(2,2));
+		JPanel pCentro = new JPanel();
+		JPanel pNombreMov = new JPanel();
+		JPanel pMovs = new JPanel();
 		JLabel imgPoke = new JLabel();
+		
 		pPoke.add(pIzq, BorderLayout.WEST);
 		pPoke.add(pCentro, BorderLayout.CENTER);
 		pEquipo.add(pPoke);
 	
-		String[] ArrayPokemons = cargarNombrePokes();
+		String[] arrayPokemons = cargarNombrePokes();
+		String[] arrayMovs = cargaNombreMovs();//CAMBIAR
 		
-		JComboBox<String> comboPoke = new JComboBox<String>(ArrayPokemons);
+		JComboBox<String> comboPoke = new JComboBox<String>(arrayPokemons);
 		pIzq.add(comboPoke, BorderLayout.NORTH);
 		
 		comboPoke.addActionListener(new ActionListener() {
@@ -71,6 +82,11 @@ public class VentanaCreadorEquipos extends JFrame {
 			}
 		});
 		
+		JComboBox<String> comboMovs = new JComboBox<String>(arrayMovs);
+		pNombreMov.add(comboMovs);
+		pCentro.add(pNombreMov, BorderLayout.NORTH);
+		pCentro.add(pMovs, BorderLayout.CENTER);
+		
 		ImageIcon icono_1 = new ImageIcon(getClass().getResource("/img/" + comboPoke.getSelectedItem() + "_frente.png"));
 		ImageIcon icono_2 = new ImageIcon(icono_1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT));
 		imgPoke.setIcon(icono_2);
@@ -78,7 +94,7 @@ public class VentanaCreadorEquipos extends JFrame {
 		
 	}
 	
-	private String[] cargarNombrePokes(){
+	public String[] cargarNombrePokes(){
 		ArrayList<String> listaPokemons = new ArrayList<String>();
 
 		File fInic = new File("src/img/");
@@ -89,13 +105,40 @@ public class VentanaCreadorEquipos extends JFrame {
 		 		}
 		 }
 		
-		String[] ArrayPokemons = new String[listaPokemons.size()];
+		String[] arrayPokemons = new String[listaPokemons.size()];
 				
 		for (int i = 0; i < listaPokemons.size(); i++) {
 				String nombrePoke = listaPokemons.get(i).replaceAll("_frente.png", "");
-				ArrayPokemons[i] = nombrePoke;
+				arrayPokemons[i] = nombrePoke;
 			}
-		return ArrayPokemons;
+		return arrayPokemons;
+	}
+	
+	public String[] cargaNombreMovs() {
+		ArrayList<String> listaMovs = new ArrayList<String>();
+		
+		try {//CAMBIAR CUANDO SE LLAMA A EST METODO Y PASARLE EL OBJETO SELECCIONADO EN LA COMBO BOX POKES.
+			Connection conn = DriverManager.getConnection(BaseDatosPoke.url);
+			Statement stmt  = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select mov1, mov2, mov3, mov4, mov5, mov6, mov7, mov8, mov9, mov10 from pokemons where name='");
+		
+			while(rs.next()) {
+				listaMovs.add(rs.getString("name").replaceAll("_", " "));
+			}
+			conn.close();
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		String[] arrayMovs = new String[listaMovs.size()];
+		
+		for (int i = 0; i < listaMovs.size(); i++) {
+				String nombrePoke = listaMovs.get(i);
+				arrayMovs[i] = nombrePoke;
+			}
+		System.out.println(arrayMovs);
+		return arrayMovs;
 	}
 	
 	private void creaPanelSup() {
@@ -133,7 +176,7 @@ public class VentanaCreadorEquipos extends JFrame {
 	
 	private static class PanelMovimiento extends JPanel {
 		
-		public PanelMovimiento() {
+		public PanelMovimiento(String nombreMov, ResultSet rs) {//TODO No sé si se puede hacer esto así
 			//COSAS A AÑADIR:
 			//Combo box arriba para elegir el movimiento y que salga también su tipo, clase, pot, prec y pp
 			//Pequeña descripción del efecto del movimiento abajo.
