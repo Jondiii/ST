1195f7fc280171fe9982b2299c7e657c44cdab69
Álvaro <detoroalvaro@opendeusto.es>
2020-01-11 13:46:08 +0100

    efecto secundarios

--- ST/src/principal/EfectoSecundario.java
+++ ST/src/principal/EfectoSecundario.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.util.Random;
+
 /**
  * Algunos movimientos pueden tener un efecto inmediato en el combate. Estos efectos son diferentes a los estados alterados
  * porque no perduran, a diferencia de los estados alterados.
@@ -12,4 +14,35 @@ package principal;
  */
 public enum EfectoSecundario {
 	RETROCESO, INMUNIDAD, RECOIL, DEBILITACION, CURACION, DESCANSAR, NULL;
+	
+	public static boolean calcularProbAtacar(Pokemon poke, Movimiento mov) {
+		boolean atacar = true;
+		Random r = new Random();
+			int aleatorio = r.nextInt(101);
+			if(aleatorio <= mov.getProb_efecto()) { atacar = false;}
+		return atacar;
+	}
+	
+	public double devuelvePS (Pokemon poke, Movimiento mov, double vidaInflinjida) {
+		double ps = 0.0;
+		switch (mov.getEfecto()) {
+		case CURACION:
+		if (mov.getNombre() == "Recuperacion" || mov.getNombre() == "Sol Matinal") ps = poke.getPs() * 0.5;
+		else {
+			ps = vidaInflinjida * 0.5;
+			}
+		break;
+		case RECOIL:
+			ps = -(vidaInflinjida * 1/3);
+			break;
+		case DESCANSAR:
+			ps = poke.getPs_max() - poke.getPs();
+			break;
+		case DEBILITACION:
+			ps = -poke.getPs();
+			break;
+		}
+		
+		return ps;
+	}
 }
